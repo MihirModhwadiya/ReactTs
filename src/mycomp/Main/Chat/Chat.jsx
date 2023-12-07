6dbdc37e9f2524bff4910f49ae2e59886bd5e2dc
MewMihStow3339 <mihirmodhwadiya21@gmail.com>
2023-12-07 10:50:31 +0530

    07-12-2023

--- src/mycomp/Main/Chat/Chat.jsx
+++ src/mycomp/Main/Chat/Chat.jsx
@@ -4,24 +4,24 @@ import Input from "./Input/Input";
 import PropTypes from "prop-types";
 
 Chat.propTypes = {
-  sidebarOpen: PropTypes.bool,
+	sidebarOpen: PropTypes.bool,
 };
 
 export default function Chat({ sidebarOpen }) {
-  return (
-    <>
-      <div className={`chat-block pt-5 pb-3`}>
-        <div className="chat-body">
-          <Messages />
-        </div>
-      </div>
-      <div
-        className={`chat-inputs btn-group bg-dark bg-opacity-25 position-fixed end-0 bottom-0 d-flex align-items-start p-3 ${
-          sidebarOpen ? "sidebar-open" : "sidebar-close"
-        }`}
-      >
-        <Input />
-      </div>
-    </>
-  );
+	return (
+		<>
+			<div className={`chat-block pt-5 pb-3`}>
+				<div className="chat-body">
+					<Messages />
+				</div>
+			</div>
+			<div
+				className={`chat-inputs btn-group bg-dark bg-opacity-25 position-fixed end-0 bottom-0 d-flex align-items-start p-3 ${
+					sidebarOpen ? "sidebar-open" : "sidebar-close"
+				}`}
+			>
+				<Input />
+			</div>
+		</>
+	);
 }
